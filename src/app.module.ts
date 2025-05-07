32d033ced1a555edd07724a3f6d88c1ee7777142
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-07 08:49:44 +0300

    petition resourcses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PetitionsModule } from './petitions/petitions.module';
+//import { PetitionsModule } from './petitions/petitions.module';
 import { PetitionsModule } from './resources/petitions/petitions.module';
+import { AuthModule } from './resources/auth/auth.module';
+import { UsersModule } from './resources/users/users.module';
 
 @Module({
-  imports: [PetitionsModule],
+  imports: [PetitionsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/resources/petitions/entities/petition.entity.ts
+++ src/resources/petitions/entities/petition.entity.ts
@@ -1,1 +1,21 @@
-export class Petition {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type PetitionDocument = Petition & Document;
+
+@Schema()
+export class Petition {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: 0 })
+  signatures: number;
+}
+
+export const PetitionSchema = SchemaFactory.createForClass(Petition);
