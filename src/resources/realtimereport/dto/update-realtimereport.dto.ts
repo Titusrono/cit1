95daab27d88afd6ff331b51a78a6b47b7085bfb7
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-09 17:01:43 +0300

    null

--- src/resources/realtimereport/dto/update-realtimereport.dto.ts
+++
@@ -1,3 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRealtimeReportDto } from './create-realtimereport.dto';
-export class UpdateRealtimereportDto extends PartialType(CreateRealtimeReportDto) {}
--- src/resources/realtimereport/realtimereport.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RealtimereportService } from './realtimereport.service';
-import { RealtimereportController } from './realtimereport.controller';
-
-@Module({
-  controllers: [RealtimereportController],
-  providers: [RealtimereportService],
-})
-export class RealtimereportModule {}
