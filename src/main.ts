5c34cbfa4997897264f1f919dc945165112a65ea
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-12 17:26:46 +0300

    change

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   // Create an instance of the app
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // ✅ Enable CORS (Cross-Origin Resource Sharing) to allow Angular frontend (http://localhost:4200)
   // to make requests to the NestJS backend (http://localhost:3000)
@@ -13,6 +15,11 @@ async function bootstrap() {
   // ✅ Enable global validation pipes (validates the DTOs)
   // This ensures that all incoming data is validated based on the rules specified in your DTOs
   app.useGlobalPipes(new ValidationPipe());
+
+  // ✅ Serve static files from the 'uploads' folder
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',  // This will allow access to images via 'http://localhost:3000/uploads/your-image.jpg'
+  });
 
   // ✅ Configure and start the app to listen on the port specified by environment variable `PORT` 
   // or fall back to 3000 if not provided.
